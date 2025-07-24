334db77ed1ee3c53e0735c0d8064d0da529dd12e
chank616 <chank616@foxmail.com>
2025-07-25 00:07:49 +0800

    feat: Accepts GET method parameters

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = FastAPI()
 
 @app.get("/test")
 def test():
-    return {"code":"0","message":"This is a test."}
+    return {"code": "0","message":"This is a test."}
+
+@app.get("/testv2")
+def testv2(name: str):
+    return {"code": "0","message": f"Hello {name}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app",host="0.0.0.0", port=8000, reload=True)
